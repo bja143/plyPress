6bd088ceb9d31b49759fa52f21fe20fa5dedd182
Suraj Patil <sapatil@live.com>
2014-11-26 20:35:01 +0530

    added barebone structure

--- plyPress.py
+++ plyPress.py
@@ -4,9 +4,44 @@ from flask.templating import render_template
 
 app = Flask('flyPress')
 
+#when the application is run for the first time it has to set up files on the system
+#this is the function used to do so
+
 @app.route('/install', methods=['GET', 'POST'])
 def install_flyPress():
-    if request == 'POST':
+    if request.method == 'POST':
         pass
     else:
-        render_template("installer.html") 
+        render_template("installer.html")
+
+#when the user logs into the system this method is executed
+@app.route('/login', methods=['GET', 'POST'])
+
+
+def valid_login(username, password):
+    pass
+
+
+def login():
+    error = None
+    if request.method=='POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return 
+    
+    
+    return render_template('login.html')
+        
+#after successful login the user is redirected to the news feed
+@app.route('/home', methods=['GET'])
+def show_feed():
+    #get data from dbms
+    render_template("home.html")
+
+#invoked while adding a new post
+@app.route('new', methods=['GET'])
+def new_post():
+    render_template('new_post.html')
+    
+    
+if __name__=='__main__':
+    app.run(debug=True)
