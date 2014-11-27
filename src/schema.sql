91b837b5ce770ac4bc8197a6b6d233a169da46a3
Suraj Patil <sapatil@live.com>
2014-11-27 21:58:31 +0530

    updated DDL

--- src/schema.sql
+++ src/schema.sql
@@ -1,24 +1,33 @@
 create database flypress;
 use flypress;
 
-
 create table user_master
 (
-    userid Varchar(5) Primary Key, 
-    doctorname Varchar(15) Not Null, 
-    password varchar(255) not null
+    email_id varchar(40) not null primary key,
+    doctorname Varchar(15) not null, 
+    password char(255) not null,
+    full_name varchar(100) not null
 ); 
+
 
 create table post_master
 (
     post_id integer primary key, 
     post_title varchar(100) not null, 
     post_content text not null,
-    timestamp varchar(50) not null
+    posted_on datetime  not null
 );
 
-create table likes(
-	post_id integer not null, 
-    constraint user_master_post_fk foreign key(post_id) references post_master(post_id);
-    post_likes integer not null
+create table comments(
+  post_id integer not null,
+  comment_id integer not null,
+  comment_content text not null,
+  email_id varchar(30) not null,
+  comment_author varchar(50) not null
 );
+
+-- create table likes(
+-- 	post_id integer not null, 
+--     constraint user_master_post_fk foreign key(post_id) references post_master(post_id),
+--     post_likes integer not null
+-- );
