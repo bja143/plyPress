154584c5e34aa7a9d5a92031fc410360e8f4c4f5
Suraj Patil <sapatil@live.com>
2014-11-30 18:01:43 +0530

    cleaning some files

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-
-
-
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/plypress'
+DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/plypress'
 DEBUG = True
 secret_key= 'developmentkey'
 USERNAME = 'admin'
