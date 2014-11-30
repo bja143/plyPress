a0247d8a0c863738f7ddf0f37b870c297a4b46dd
Suraj Patil <sapatil@live.com>
2014-11-30 17:04:38 +0530

    created a run.py file to run the app, and renamed database.py to models.py and plyPress.py to views.py

---
+++ config.py
@@ -0,0 +1,9 @@
+
+
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/plypress'
+DEBUG = True
+secret_key= 'developmentkey'
+USERNAME = 'admin'
+PASSWORD = 'default'
+TITLE='MyTechbook'
---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import models
+import views
+
+#create the application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+if __name__=='__main__':
+    app.run(debug=True)
+
