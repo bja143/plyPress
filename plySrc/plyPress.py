68b5302a95d256def54e9f6553d5dc06afde0ff2
Suraj Patil <sapatil@live.com>
2014-12-02 19:05:27 +0530

    added sessions

--- plySrc/plyPress.py
+++ plySrc/plyPress.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, url_for, render_template, session
-
+from flask import Flask, request, url_for, render_template, session, redirect
 from plySrc.models import fetch_posts, insert_post, fetch_post_by_id
 from plySrc import app
-from plySrc.config import PASSWORD, USERNAME, TITLE
+from plySrc.config import PASSWORD, USERNAME, TITLE, secret_key
 
+app.secret_key = secret_key
 
 #implement the installer later
 
@@ -20,16 +20,23 @@ from plySrc.config import PASSWORD, USERNAME, TITLE
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method=='POST':
+    if request.method=='POST' and session['logged_in'] == False:
         if valid_login(request.form['username'], request.form['password']):
-            #session['logged_in'] = True
+            session['logged_in'] = True
             return render_template("dashboard.html")
         else:
             error = 'Invalid credentials'
             return render_template('dashboard.html', error=error)
+    elif session['logged_in']==True:
+        redirect(url_for(dashboard, 400))
     else:
         return render_template('login.html', title=TITLE)
 
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop(USERNAME)
+    session['logged_in'] = False
+    
 def valid_login(username, password):
     if username== USERNAME and password==PASSWORD:
         return True
@@ -51,25 +58,29 @@ def show_feed():
 #invoked while adding a new post
 @app.route('/new', methods=['GET', 'POST'])
 def new_post():
-    if request.method=='GET':
-        return render_template('post.html')
+    if session['logged_in']==True:
+        
+        if request.method=='GET':
+            return render_template('post.html')
+        else:
+            title= request.form['title']
+            subtitle= request.form['subtitle']
+            text= request.form['text']
+            username = USERNAME
+            from datetime import date
+            to = date.today()
+            date=to.strftime("%B %d %Y")
+            
+            post = {
+                    'title':title, 'subtitle':subtitle, 'text':text,
+                    'date':date, 'username':username              
+                    }
+            
+            insert_post(post)
+            
+            render_template('post.html', post=post)
     else:
-        title= request.form['title']
-        subtitle= request.form['subtitle']
-        text= request.form['text']
-        username = USERNAME
-        from datetime import date
-        to = date.today()
-        date=to.strftime("%B %d %Y")
-        
-        post = {
-                'title':title, 'subtitle':subtitle, 'text':text,
-                'date':date, 'username':username              
-                }
-        
-        insert_post(post)
-        
-        render_template('post.html', post=post)
+        redirect(url_for(login, 403))
         
 
 @app.route('/contact', methods=['GET'])
@@ -80,9 +91,12 @@ def contact():
 def about():
     return render_template("about.html", title=TITLE)
 
-# @app.route('/dashboard', methods=['GET'])
-# def dashboard():
-#     return render_template("dashboard.html")
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    if session['logged_in']==True:
+        return render_template("dashboard.html")
+    else:
+        redirect(url_for(login), 403)
 
 @app.route('/post/<postid>')
 def show_post(postid):
